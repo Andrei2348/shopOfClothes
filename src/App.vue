af651652a38ae4e0f8a68592b4a06bc36b413a20
Andrei2348 <makarevichandco@gmail.com>
2024-04-29 13:13:37 +0300

    Начало проекта интернет магазина (учебное)

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!-- @format -->
+
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+
+function increment() {
+  count.value++;
+  console.log(count);
+}
+
+function updateCount(event) {
+  count.value = Number(event.target.value);
+}
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">+</button>
+  <!-- <input @input="updateCount"> -->
+  <input v-model="count" />
+</template>
+
+<style scoped></style>
