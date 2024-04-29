839af8a4f1d378c911a64fba27620b132e208885
Andrei2348 <makarevichandco@gmail.com>
2024-04-29 16:28:06 +0300

    Создание карточки

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,113 @@
 
 <script setup>
 import { ref } from 'vue';
-const count = ref(0);
+import Header from './components/Header.vue';
+// const count = ref(0);
 
-function increment() {
-  count.value++;
-  console.log(count);
-}
+// function increment() {
+//   count.value++;
+//   console.log(count);
+// }
 
-function updateCount(event) {
-  count.value = Number(event.target.value);
-}
+// function updateCount(event) {
+//   count.value = Number(event.target.value);
+// }
 </script>
 
 <template>
-  <h1>{{ count }}</h1>
+  <!-- <h1>{{ count }}</h1>
   <button @click="increment">+</button>
-  <!-- <input @input="updateCount"> -->
-  <input v-model="count" />
+   <input @input="updateCount">
+  <input v-model="count" /> -->
+
+  <main class="main">
+    <Header />
+
+    <div class="container">
+      <div class="card">
+        <div class="card__image-container">
+          <img class="card__image" src="/public/products/hudi.jpg" alt="hudi" />
+          <div class="card__status">New</div>
+          <img
+            class="card__heart"
+            src="/public/icons/card_heart.svg"
+            alt="Heart"
+          />
+        </div>
+        <div class="card__content">
+          <p class="card__price">3990 ₽</p>
+          <p class="card__brand">Stone Island</p>
+          <p class="card__title">Зип худи шерпа с капюшоном (белый)</p>
+          <img class="card__bag" src="/public/icons/card_bag.svg" alt="Bag" />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+}
+.card__image {
+  display: block;
+}
+.card__image-container,
+.card__content {
+  position: relative;
+}
+
+.card__status {
+  font-size: 14px;
+  line-height: 1;
+  font-weight: 600;
+  color: #fff;
+  padding: 5px 11px;
+  background-color: #13ed1c;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+.card__heart {
+  position: absolute;
+  display: block;
+  height: 34px;
+  width: 34px;
+  right: 8px;
+  top: 20px;
+  cursor: pointer;
+  z-index: 10;
+}
+.card__image-container{
+  margin-bottom: 20px;
+
+}
+.card__price{
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 1;
+  color: #1D1D1D;
+  margin-bottom: 20px;
+}
+.card__brand,
+.card__title{
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1;
+  color: #1D1D1D;
+}
+.card__brand{
+  margin-bottom: 10px;
+}
+.card__bag{
+  display: block;
+  height: 24px;
+  width: 24px;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,93 @@
+<!-- @format -->
+
+<script setup></script>
+
+<template>
+  <header class="header">
+    <div class="header__title">
+      <div class="container">
+        <h1 class="header__title-text">
+          Lorem ipsum is placeholder text commonly used -20%
+        </h1>
+      </div>
+    </div>
+    <div class="header__wrapper">
+      <div class="container">
+        <div class="header__content">
+          <img class="header__logo" src="/public/icons/logo.svg" alt="" />
+          <ul class="header__menu-items">
+            <li class="header__menu-item">Sale</li>
+            <li class="header__menu-item">Женщинам</li>
+            <li class="header__menu-item">Мужчинам</li>
+            <li class="header__menu-item">Детям</li>
+          </ul>
+          <ul class="header__profile-items">
+            <li class="header__profile-item">
+              <img src="/public/icons/search.svg" alt="Search" />
+            </li>
+            <li class="header__profile-item">
+              <img src="/public/icons/user.svg" alt="User" />
+            </li>
+            <li class="header__profile-item">
+              <img src="/public/icons/heart.svg" alt="Heart" />
+            </li>
+            <li class="header__profile-item">
+              <img src="/public/icons/bag.svg" alt="Bag" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+}
+.header__title-text {
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+  padding: 18px 0 20px;
+  color: #565656;
+  text-align: center;
+  text-transform: uppercase;
+}
+.header__wrapper {
+  background-color: #1d1d1d;
+}
+.header__logo {
+  height: 24px;
+  width: 53px;
+  margin-right: 103px;
+}
+.header__menu-items,
+.header__profile-items {
+  display: flex;
+  align-items: center;
+}
+.header__profile-items {
+  gap: 20px;
+}
+.header__menu-item {
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1;
+  color: #fff;
+  text-transform: uppercase;
+  padding: 29px 34px;
+}
+.header__content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.header__profile-item,
+.header__menu-item {
+  cursor: pointer;
+}
+</style>
